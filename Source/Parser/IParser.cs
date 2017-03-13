5458a6fc1a92f65b886527459beebaa99918c398
Paolo Nicodemo <paolonicodemo@gmail.com>
2017-03-13 22:58:11 +0100

    added generic interfaces

---
+++ Source/Parser/IParser.cs
@@ -0,0 +1,18 @@
+﻿namespace Parser
+{
+    using Model;
+    using System.IO;
+
+    /// <summary>
+    ///     base parser interface, takes a string returns the parsed type
+    /// </summary>
+    public interface IParser
+    {
+        /// <summary>
+        ///     Parses a string constructing the type representation
+        /// </summary>
+        /// <param name="text">Text to parse</param>
+        /// <returns>Parsed type</returns>
+        TypeProperty Parse(string text);
+    }
+}
---
+++ Source/Parser/IPreProcessor.cs
@@ -0,0 +1,15 @@
+﻿namespace Parser
+{
+    /// <summary>
+    ///     Preprocesses a block of text to replace or execute macros before parsing
+    /// </summary>
+    public interface IPreProcessor
+    {
+        /// <summary>
+        ///     Preprocesses a string of text
+        /// </summary>
+        /// <param name="input">Input to preprocess</param>
+        /// <returns>Output to parse</returns>
+        string Preprocess(string input);
+    }
+}
